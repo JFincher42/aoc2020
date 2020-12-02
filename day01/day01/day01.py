c7d0a365ac3b60cbd21a4e20d34a26cb55a85a05
Jon Fincher <jfincher42@gmail.com>
2020-12-01 19:28:47 -0600

    Day 01 fixed output

--- day01/day01/day01.py
+++ day01/day01/day01.py
@@ -1,33 +1,48 @@
 # AOC 2020 Day 1
 
+import pathlib
+
+root_path = pathlib.Path.home() / "git" / "aoc2020" / "day01" / "day01"
+
 
 def part1():
 
-    with open("day01/day01/input", "r") as f:
+    with open(root_path / "input", "r") as f:
         numbers = [int(line) for line in f.readlines()]
 
     for first in numbers:
         second = 2020 - first
         if second in numbers:
-            print(f"Part 1: {first} and {second}, product is {first*second}") 
+            print(f"Part 1: {first} and {second}, product is {first*second}")
+            return
+
+    # If we get here, we have a problem
+    print("PROBLEM")
+    return
+
 
 def check_sum(target_sum, first_number, numbers):
     for second in numbers:
         if second != first_number:
             third = target_sum - second
             if third > 0 and third in numbers:
-                print(f"Part 2: {first_number}, {second}, and {third}, product is {first_number*second*third}.")
+                print(
+                    f"Part 2: {first_number}, {second}, and {third}, product is {first_number*second*third}"
+                )
                 return True
     return False
 
 
 def part2():
-    with open("day01/day01/input", "r") as f:
+    with open(root_path / "input", "r") as f:
         numbers = [int(line) for line in f.readlines()]
 
     for first in numbers:
-        if not check_sum(2020-first, first, numbers):
-            print("PROBLEM")
+        if check_sum(2020 - first, first, numbers):
+            return
+
+    # If we get here, there is a problem
+    print("PROBLEM")
 
 
 if __name__ == "__main__":
