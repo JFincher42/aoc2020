6e0ab37b7b53f43d9e6ce5a7304586c78c220388
Jon Fincher <jfincher42@gmail.com>
2020-12-10 16:30:54 -0600

    Still too fucking stupid for part 2

--- day10/day10/day10.py
+++ day10/day10/day10.py
@@ -26,73 +26,50 @@ def part1(numbers):
     return steps[0] * steps[2]
 
 
-def count_paths(first, second):
-    if first == 1:
-        return 0
+def count_paths(numbers, index):
 
-    if second == 1:
-        return first
+    total_paths = 1
+    while index < len(numbers):
 
-    return first * second
+        # How many ways out from here
+        paths_out = [0,0,0]
+        for i in range(3):
+            if numbers[index]+i+1 in numbers:
+                paths_out[i] = 1
+
+        if sum(paths_out) == 3:
+            # Now we need to figure out every path out
+            total_paths += count_paths(numbers, index+1)
+            total_paths += count_paths(numbers, index+2)
+            total_paths += count_paths(numbers, index+3)
+            index += 3
+
+        elif sum(paths_out) == 2:
+            if paths_out[0]:
+                total_paths += count_paths(numbers, index+1)
+            if paths_out[1]:
+                total_paths += count_paths(numbers, index+2)
+            if paths_out[2]:
+                total_paths += count_paths(numbers, index+3)
+            index += 2
+
+        # Only one way out, just keep looping
+        else:
+            index += 1
+
+    return total_paths
 
 
 def part2(numbers):
-    complete = set(numbers)
-    complete.add(max(numbers) + 3)
-    complete.add(0)
+    numbers.append(0)
+    numbers.append(max(numbers)+3)
+    numbers.sort()
 
-    sorted = list(complete)
-
-    diffs = [3]
-    for i, plug in enumerate(sorted):
-        if i < len(sorted) - 1:
-            diffs.append(sorted[i + 1] - plug)
-
-    print(sorted)
-    print(diffs)
-
-    i = 0
-    single_count = 0
-    total = 0
-    while i < len(diffs):
-        if diffs[i] == 3:
-            if single_count>=3:
-                total += factorial(single_count) / factorial(single_count - 3)
-            else:
-                total += single_count
-                single_count = 0
-        if diffs[i] == 1:
-            single_count += 1
-        i += 1
-    return total
-
-    # path_count = []
-    # for plug in complete:
-    #     paths = [0,0,0]
-    #     for i in range(3):
-    #         if plug+i+1 in complete:
-    #             paths[i] = 1
-
-    #     if sum(paths) > 0:
-    #         path_count.append(sum(paths))
-
-    # print(path_count)
-    # # return sum(path_count) + 1
-    # # return reduce(count_paths, path_count)
-
-    # final_count = 0
-    # for index, path_length in enumerate(path_count):
-    #     if path_length == 3:
-    #         final_count += path_length * path_count[index+1]
-    #     elif path_length == 2:
-    #         final_count += path_length
-
-    # return final_count
-
+    return count_paths(numbers, 0)
 
 if __name__ == "__main__":
 
-    with open(root_path / "sample1", "r") as f:
+    with open(root_path / "sample2", "r") as f:
         numbers = [int(line) for line in f.readlines()]
 
     print(f"Part 1: Number: {part1(numbers)}")
