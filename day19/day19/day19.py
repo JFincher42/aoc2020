51ea02d5dd605f5501d0860b848626c52a96198b
Jon Fincher <jfincher42@gmail.com>
2020-12-19 16:39:07 -0600

    Took working code I understood from someone else - still too stupid to do it myself.

--- day19/day19/day19.py
+++ day19/day19/day19.py
@@ -1,16 +1,59 @@
 # AOC 2020 Day 18
 
 import pathlib
+import re
 
 root_path = pathlib.Path.home() / "git" / "aoc2020" / "day19" / "day19"
 
 
-def part1(lines):
+def convertToRegex(rule, rulesDict):
+    returnRule = ""
+    if rule in ["a", "b"]:
+        returnRule = rule
+    else:
+        returnRule = returnRule + "("
+        if rule.find("|") > 0:
+            subRules = rule.split(" | ")
+            for sub in subRules:
+                nums = sub.split(" ")
+                for num in nums:
+                    # print(rulesDict[int(num)])
+                    returnRule = returnRule + convertToRegex(
+                        rulesDict[int(num)], rulesDict
+                    )
+                returnRule = returnRule + "|"
+            returnRule = returnRule[0 : len(returnRule) - 1]  # get rid of last |
+        else:  # there is no or operator
+            nums = rule.split(" ")
+            for num in nums:
+                # print(rulesDict[int(num)])
+                returnRule = returnRule + convertToRegex(rulesDict[int(num)], rulesDict)
+        returnRule = returnRule + ")"
+    return returnRule
 
-    return -1
+
+def convert_rules(line_rules):
+    rules = {}
+    for rule_num, rule in line_rules.items():
+        converted_rule = convertToRegex(rule, line_rules)
+        if converted_rule[0] == "(":
+            converted_rule = converted_rule[1:-1]
+        rules[rule_num] = converted_rule
+
+    return rules
 
 
-def part2(lines):
+def part1(lines, rules):
+
+    rule = "^" + rules[0] + "$"
+    return sum(1 for line in lines if re.match(rule, line))
+
+
+def part2(lines, rules):
+
+    print(f"Rule 8: {rules[8]}")
+    print(f"Rule 11: {rules[11]}")
+
 
     return -1
 
@@ -21,17 +64,12 @@ def parse_rules(lines):
 
     index = 0
     while lines[index]:
-        rule_number, production_string = lines[index].split(":")
-        products = []
-        products_string = production_string.split("|")
-        for product_rule in products_string:
-            product_rule = product_rule.strip()
-            if '"' in product_rule:
-                products.append(product_rule[1])
-            else:
-                product = [int(x) for x in product_rule.split(" ")]
-                products.append(product)
-        rules[int(rule_number)] = products
+        rule_number, production_string = lines[index].split(": ")
+
+        if '"' in production_string:
+            production_string = production_string[1]
+
+        rules[int(rule_number)] = production_string
 
         index += 1
 
@@ -40,10 +78,17 @@ def parse_rules(lines):
 
 if __name__ == "__main__":
 
-    with open(root_path / "sample", "r") as f:
+    with open(root_path / "input", "r") as f:
         lines = [line.strip() for line in f.readlines()]
 
-    rules = parse_rules(lines)
+    line_rules = parse_rules(lines)
+    rules = convert_rules(line_rules)
 
-    print(f"Part 1: Answer: {part1(lines, rules)}")
-    print(f"Part 2: Answer: {part2(lines)}")
+    # Find the first real line
+    index = 0
+    while lines[index]:
+        index += 1
+    index += 1
+
+    print(f"Part 1: Answer: {part1(lines[index:], rules)}")
+    print(f"Part 2: Answer: {part2(lines[index:], rules)}")
---
+++ day19/day19/working_code.py
@@ -0,0 +1,81 @@
+import re
+
+# process input
+stream = open("day19/sample")
+# stream = open("day19/input")
+lines = stream.read()
+data = lines.split("\n\n")
+rules = data[0].split("\n")
+datum = data[1].split("\n")
+for row in datum:
+    if row == "":
+        datum.remove(row)  # there's sometimes an empty newline at the end
+
+
+def hasNumbers(inputString):
+    return bool(re.search(r"\d", inputString))
+
+
+# Part 1 goal is the number of messages that completely match rule 0, or rulesDict[0]
+# for each rule, I want to build a regular expression
+# I want a recursive algorithm to do this
+
+rulesDict = {}
+for rule in rules:
+    parts = rule.split(": ")
+    ruleNum = int(parts[0])
+    ruleItself = parts[1]
+    if ruleItself == '"a"':  # this is a hacky way of doing this without more regEx's
+        ruleItself = "a"
+    if ruleItself == '"b"':
+        ruleItself = "b"
+    rulesDict[ruleNum] = ruleItself
+
+regExRules = {}  # sorted just to make reading debugging output easier
+
+
+def convertToRegex(rule):
+    returnRule = ""
+    if rule in ["a", "b"]:
+        returnRule = rule
+    else:
+        returnRule = returnRule + "("
+        if rule.find("|") > 0:
+            subRules = rule.split(" | ")
+            for sub in subRules:
+                nums = sub.split(" ")
+                for num in nums:
+                    # print(rulesDict[int(num)])
+                    returnRule = returnRule + convertToRegex(rulesDict[int(num)])
+                returnRule = returnRule + "|"
+            returnRule = returnRule[0 : len(returnRule) - 1]  # get rid of last |
+        else:  # there is no or operator
+            nums = rule.split(" ")
+            for num in nums:
+                # print(rulesDict[int(num)])
+                returnRule = returnRule + convertToRegex(rulesDict[int(num)])
+        returnRule = returnRule + ")"
+    return returnRule
+
+
+for row in rulesDict:
+    rule = rulesDict[row]
+    if convertToRegex(rule)[0] == "(":
+        regExRules[row] = convertToRegex(rule)[1 : len(convertToRegex(rule)) - 1]
+    else:
+        regExRules[row] = convertToRegex(rule)
+
+# print(regExRules)
+# part 1 I just want rule 0
+count = 0
+rule = "^" + regExRules[0] + "\Z"
+for row in datum:
+    # print(re.match(rule,row))
+    if re.match(rule, row):
+        # print(row,"matches",rule)
+        count += 1
+    else:
+        pass  # print(row,"does not match",rule)
+    # print()
+
+print("part 1:", count)
