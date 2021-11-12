0bcefaa1f12340b18ab025b0aefa04169e2a0e2e
Jon Fincher <jfincher42@gmail.com>
2021-11-11 21:24:30 -0600

    Day 25 part 1 done

--- day25/day25/day25.py
+++ day25/day25/day25.py
@@ -1,26 +1,51 @@
-# AOC 2020 Day 18
+# AOC 2020 Day 25
 
 import pathlib
 
-root_path = pathlib.Path.home() / "git" / "aoc2020" / "day19" / "day19"
+root_path = pathlib.Path.home() / "git" / "aoc2020" / "day25" / "day25"
 
 
+def find_loop(pk):
+    sn = 7
+    loop = 0
+    val = 1
+
+    while val != pk:
+        val *= sn
+        val %= 20201227
+        loop += 1
+
+    return loop
 
 
-def part1(lines):
+def transform(sn, loop):
+    val = 1
+    for _ in range(loop):
+        val *= sn
+        val %= 20201227
 
-    return -1
+    return val
 
-def part2(lines):
 
+def part1(cpk, dpk):
+    card_loop = find_loop(cpk)
+    door_loop = find_loop(dpk)
+    return transform(cpk, door_loop)
+
+
+def part2(cpk, dpk):
     return -1
 
 
 if __name__ == "__main__":
 
-    with open(root_path / "input", "r") as f:
-        lines = [line.strip() for line in f.readlines()]
+    card_public_key = 11562782
+    door_public_key = 18108497
 
+    sample_card_public_key = 5764801
+    sample_door_public_key = 17807724
 
-    print(f"Part 1: Answer: {part1(lines)}")
-    print(f"Part 2: Answer: {part2(lines)}")
+    print(f"Part 1: Answer: {part1(card_public_key, door_public_key)}")
+    # print(f"Part 1: Answer: {part1(sample_card_public_key, sample_door_public_key)}")
+    print(f"Part 2: Answer: {part2(card_public_key, door_public_key)}")
+    # print(f"Part 2: Answer: {part2(sample_card_public_key, sample_door_public_key)}")
